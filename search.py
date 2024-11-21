d16a5404ef2238575a7672aa73c8a11ae9d401f3
jv24601 <108680838+jv24601@users.noreply.github.com>
2024-11-21 03:04:15 -0600

    make files more efficient

--- search.py
+++ search.py
@@ -2,17 +2,21 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+from scipy import sparse
+import joblib
 
-jazz_df = pd.read_csv('jazz.csv')
+jazz_df = pd.read_csv('jazz2.csv')
 
-vectorizer = TfidfVectorizer(max_features=2000,stop_words='english',min_df =.05,max_df=.95)
-vectors = vectorizer.fit_transform(jazz_df['text']) 
+vectorizer = joblib.load('vectorizer.pkl')
+vectors = sparse.load_npz('jazz.npz')
 dense_vectors = vectors.todense().tolist()
 
 nbrs = NearestNeighbors(n_neighbors=10,algorithm='brute')
 nbrs.fit(dense_vectors)
 
-term_df = pd.DataFrame(dense_vectors,columns=vectorizer.get_feature_names_out())
+names = np.load('names.npy',allow_pickle=True)
+
+term_df = pd.DataFrame(dense_vectors,columns=names)
 
 def jazz_search(search_string):
     _,indices = nbrs.kneighbors(vectorizer.transform([search_string]))
